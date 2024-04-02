65a6571a2bb01569e20573404f6d666ba74f7465
zeerkius <90996736+zeerkius@users.noreply.github.com>
2024-04-02 15:59:51 -0500

    Add files via upload
    This is the Java code we will be using to create a connection between our interface and our database. This has been created using a online tutorial. From the knowledge gained so far as well as the code, we will have an object which will be our connection. Lets call it a plug and the actual connection function takes our host, ip , password and ect and within that use it to accces our database with a connection function

--- Main_RooME_Connection.java
+++ Main_RooME_Connection.java
@@ -1,3 +1,20 @@
-public class Main {
-    
+package com.example.connectandroid_sql;
+
+import...
+
+public class Main_Connection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstance){
+         super.onCreate(SavedInstanceState);
+         setContentView(R.layout.activity_main);
+     }
+
+
+
 }
+
+
+
+
+
